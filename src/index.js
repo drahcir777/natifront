d1561812161dd4c371228a6c06f652f6b2c0a706
Drahcir777 <richa.freire@gmail.com>
2021-01-14 23:38:26 -0300

    bb

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const WidgetDivs = document.querySelectorAll('.reddit_widget')
-WidgetDivs.forEach(Div => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App domElement={Div} />
-    </React.StrictMode>,
-    Div
-  );
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
